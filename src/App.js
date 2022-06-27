3f0d92a67e8e33a9e53d2b41dcc4a51257443b7e
dormirMais <74685291+dormirMais@users.noreply.github.com>
2022-06-27 15:15:53 +0900

    #2.6 Recap

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const rotationAnimation = keyframes`
+    0%{
+        transform:rotate(0deg);
+        border-radius: 10px;
+    }50%{
+        transform:rotate(360deg);
+        border-radius: 100px
+    }100%{
+        transform:rotate(0deg);
+        border-radius: 10px;
+    }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
 `;
 
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.h1`
-  color: white;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Btn = styled.button`
-  color: white;
+  width: 200px;
+  height: 200px;
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
+  animation: ${rotationAnimation} 2s linear infinite;
 
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${Emoji} {
+    &:hover {
+      font-size: 98px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>😊</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
